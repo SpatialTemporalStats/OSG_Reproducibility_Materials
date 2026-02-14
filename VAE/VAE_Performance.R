3ce120e40aa83724ffe1db8b3fa048a43fe01c01
SpatialTemporalStats <168986283+SpatialTemporalStats@users.noreply.github.com>
2026-02-13 17:20:38 -0800

    Add files via upload

---
+++ VAE/VAE_Performance.R
@@ -0,0 +1,105 @@
+################################################################################
+# This file includes all steps to reproduce Figure S12                         #
+################################################################################
+# Necessary packages
+# library(reticulate)
+np=import("numpy")
+# library(ggplot2)
+# library(maps)
+# source(here("Data","Data_Treatment.R"))
+
+
+# Necessary ARP information for ploting figures
+world_map=map_data("world")                                                   # Extract the map data for the region
+arabian_peninsula_countries=c("Saudi Arabia", "Yemen", 
+                              "Oman", "United Arab Emirates", 
+                              "Qatar", "Bahrain", "Kuwait")                # Define the countries of the Arabian Peninsula
+arabian_peninsula=subset(world_map, region %in% arabian_peninsula_countries)  # Filter the map data for the Arabian Peninsula countries
+dataARP=data.frame(lon=rep(arabian_peninsula$long,times=2),lat=rep(arabian_peninsula$lat,times=2),
+                   type=rep(arabian_peninsula$group,times=2),
+                   group=as.factor(rep(c("U","V"),each=length(arabian_peninsula$long))))
+
+
+
+###### Part 1. Load the performance of VAE trained by data from the first month
+x=np$load(here("VAE","test_recon_loss_1m.npy"))
+xu=c(t(x[,,1]))[id.ARP]
+xv=c(t(x[,,2]))[id.ARP]
+dataF=data.frame(lon=rep(Dat.loc.arp[,1],times=2),lat=rep(Dat.loc.arp[,2],times=2),
+                 RMSE=c(sqrt(xu),sqrt(xv)),
+                 group=as.factor(rep(c("U","V"),each=nrow(Dat.loc.arp))))
+PT=ggplot()+facet_wrap(~ group, ncol = 2)+
+  geom_raster(mapping=aes(lon, lat, fill = RMSE),data=dataF)+
+  geom_path(mapping=aes(x = lon, y = lat, group = type), data=dataARP,size=0.3,color="darkgray")+
+  scale_fill_gradient2(low = "white",high = "#E41A1C",limits=c(0,7.6))+
+  scale_y_continuous(limits=c(12,30))+
+  theme_bw()+theme(panel.grid.major = element_blank(),
+                   panel.grid.minor = element_blank(),
+                   panel.background = element_rect(colour = "black"),
+                   axis.text=element_text(size=12),
+                   axis.title = element_text(size=12),
+                   #legend.justification = c(0,0),
+                   legend.position ="right",
+                   legend.title = element_text(),
+                   legend.key.width=unit(0.5,"line"),
+                   legend.key.height=unit(0.7,"line"))+
+  labs(fill=expression(ms^{-1}))+ylab("Latitude")+xlab("Longitude")
+print(PT)          # Save to 6.30*3.00 VAE_Perform_1m.pdf
+
+
+
+
+###### Part 2. Load the performance of VAE trained by data from the first year
+x=np$load(here("VAE","test_recon_loss_1y.npy"))
+xu=c(t(x[,,1]))[id.ARP]
+xv=c(t(x[,,2]))[id.ARP]
+dataF=data.frame(lon=rep(Dat.loc.arp[,1],times=2),lat=rep(Dat.loc.arp[,2],times=2),
+                 RMSE=c(sqrt(xu),sqrt(xv)),
+                 group=as.factor(rep(c("U","V"),each=nrow(Dat.loc.arp))))
+PT=ggplot()+facet_wrap(~ group, ncol = 2)+
+  geom_raster(mapping=aes(lon, lat, fill = RMSE),data=dataF)+
+  geom_path(mapping=aes(x = lon, y = lat, group = type), data=dataARP,size=0.3,color="darkgray")+
+  scale_fill_gradient2(low = "white",high = "#E41A1C",limits=c(0,1.7))+
+  scale_y_continuous(limits=c(12,30))+
+  theme_bw()+theme(panel.grid.major = element_blank(),
+                   panel.grid.minor = element_blank(),
+                   panel.background = element_rect(colour = "black"),
+                   axis.text=element_text(size=12),
+                   axis.title = element_text(size=12),
+                   #legend.justification = c(0,0),
+                   legend.position ="right",
+                   legend.title = element_text(),
+                   legend.key.width=unit(0.5,"line"),
+                   legend.key.height=unit(0.7,"line"))+
+  labs(fill=expression(ms^{-1}))+ylab("Latitude")+xlab("Longitude")
+print(PT)          # Save to 6.30*3.00 VAE_Perform_1y.pdf
+
+
+
+
+###### Part 3. Load the performance of VAE trained by data from the first nine years
+x=np$load(here("VAE","test_recon_loss_9y.npy"))
+xu=c(t(x[,,1]))[id.ARP]
+xv=c(t(x[,,2]))[id.ARP]
+dataF=data.frame(lon=rep(Dat.loc.arp[,1],times=2),lat=rep(Dat.loc.arp[,2],times=2),
+                 RMSE=c(sqrt(xu),sqrt(xv)),
+                 group=as.factor(rep(c("U","V"),each=nrow(Dat.loc.arp))))
+PT=ggplot()+facet_wrap(~ group, ncol = 2)+
+  geom_raster(mapping=aes(lon, lat, fill = RMSE),data=dataF)+
+  geom_path(mapping=aes(x = lon, y = lat, group = type), data=dataARP,size=0.3,color="darkgray")+
+  scale_fill_gradient2(low = "white",high = "#E41A1C",limits=c(0,1.7))+
+  scale_y_continuous(limits=c(12,30))+
+  theme_bw()+theme(panel.grid.major = element_blank(),
+                   panel.grid.minor = element_blank(),
+                   panel.background = element_rect(colour = "black"),
+                   axis.text=element_text(size=12),
+                   axis.title = element_text(size=12),
+                   #legend.justification = c(0,0),
+                   legend.position ="right",
+                   legend.title = element_text(),
+                   legend.key.width=unit(0.5,"line"),
+                   legend.key.height=unit(0.7,"line"))+
+  labs(fill=expression(ms^{-1}))+ylab("Latitude")+xlab("Longitude")
+print(PT)          # Save to 6.30*3.00 VAE_Perform_9y.pdf
+
+
---
+++ VAE/Wind_VAE_Tuning_Parameters.py
@@ -0,0 +1,328 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#%% Document
+# Two-Stage Approach: we assume that spatial data (or images) at different time points are independent, first modeling the spatial structure using a variational autoencoder (VAE), and then modeling the temporal dynamics of the latent variables across time.
+# This file tunes the VAE architecture by shuffling spatial images across different time points, splitting the data into training and validation sets, and evaluating model performance using the validation reconstruction loss, validation total loss.
+# Tunning follows the order as below:
+# 1) latent dimension: 8, 16, 32, 64, 128 --> 64
+# 2) decoder width: 16, 32, 64, 96, 128 --> 96
+# 3) beta for balancing regularization and KL: 0.4, 0.5, 0.7, 1, 3 --> 0.7
+# 4) learning rate: 1e-3, 5e-4, 1e-4, 5e-5 --> 1e-4
+# 5) number of filters: (32,64), (64, 128), (64,256) --> (32,64)
+# 6) dense: 64, 128, 256, 512, 1024, 1280 --> 1024
+# 7) batch size: 32, 64, 128 --> 32
+
+
+#%% Setup
+import os                                      # access to environment variables and file paths
+# os.environ["TF_USE_CUDNN_AUTOTUNE"] = "0"
+os.environ["KERAS_BACKEND"] = "tensorflow"     # tell keras to use tensorflow as the backend
+
+import numpy as np
+import tensorflow as tf
+# Disable XLA compilation completely
+# tf.config.optimizer.set_jit(False)
+import keras
+from keras import ops
+from keras import layers                       # brings in buiding blocks
+# import Data_Treatment
+import matplotlib.pyplot as plt
+import xarray as xr
+import pandas as pd
+from matplotlib.path import Path
+
+
+#%% Import and preprocess the data
+# Read data from 2014 into Wind_ARP
+daT=xr.open_dataset("Data/WindSpeeduv_ARP_2014.nc")
+lon=daT.variables["longitude"][:]   # Longitude
+lat=daT.variables["latitude"][:]    # Latitude
+Dat_loc=np.column_stack([np.tile(lon,len(lat)),np.repeat(lat,len(lon))]) # Locations
+R=len(daT.variables["number"][:])   # Numer of ensembles
+NT=len(daT.variables["time"][:])    # Number of time points in 2014
+Dat_ru=daT.variables["u10"][:]      # u-component (10m)
+Dat_rv=daT.variables["v10"][:]      # v-component (10m)
+Dat_ru=Dat_ru.values.astype("float32")    # Tarnsfer to numpy array
+Dat_rv=Dat_rv.values.astype("float32")
+Dat_ru=Dat_ru.reshape(-1,37,55)           # Flatten the first two dimensions (time, ensemble) by iterating all ensembles at each time point
+Dat_ru=np.expand_dims(Dat_ru,axis=-1)     # Add one more dimension of dimension 1 (add channel)
+Dat_rv=Dat_rv.reshape(-1,37,55)
+Dat_rv=np.expand_dims(Dat_rv,axis=-1)
+Wind_ARP=np.concatenate([Dat_ru,Dat_rv],axis=3) # Put U and V variables into two channels
+
+# Read data from 2015 into Wind_ARP
+daT=xr.open_dataset("Data/WindSpeeduv_ARP_2015.nc")
+Dat_ru=daT.variables["u10"][:]      
+Dat_rv=daT.variables["v10"][:]      
+Dat_ru=Dat_ru.values.astype("float32")  
+Dat_rv=Dat_rv.values.astype("float32")
+Dat_ru=Dat_ru.reshape(-1,37,55)   
+Dat_rv=Dat_rv.reshape(-1,37,55)
+Dat_ru=np.expand_dims(Dat_ru,axis=-1)
+Dat_rv=np.expand_dims(Dat_rv,axis=-1)
+Dat_ru=np.concatenate([Dat_ru,Dat_rv],axis=3)
+Wind_ARP=np.concatenate([Wind_ARP,Dat_ru],axis=0)  
+
+# Read data from 2016 into Wind_ARP
+daT=xr.open_dataset("Data/WindSpeeduv_ARP_2016.nc")
+Dat_ru=daT.variables["u10"][:]      
+Dat_rv=daT.variables["v10"][:]      
+Dat_ru=Dat_ru.values.astype("float32")  
+Dat_rv=Dat_rv.values.astype("float32")
+Dat_ru=np.delete(Dat_ru,np.s_[472:480],axis=0)      # (31+28)*8+1=473
+Dat_rv=np.delete(Dat_rv,np.s_[472:480],axis=0)      # #(31+29)*8=480
+Dat_ru=Dat_ru.reshape(-1,37,55)   
+Dat_rv=Dat_rv.reshape(-1,37,55)
+Dat_ru=np.expand_dims(Dat_ru,axis=-1)
+Dat_rv=np.expand_dims(Dat_rv,axis=-1)
+Dat_ru=np.concatenate([Dat_ru,Dat_rv],axis=3)
+Wind_ARP=np.concatenate([Wind_ARP,Dat_ru],axis=0) 
+
+# Read data from 2017 into Wind_ARP
+daT=xr.open_dataset("Data/WindSpeeduv_ARP_2017.nc")
+Dat_ru=daT.variables["u10"][:]      
+Dat_rv=daT.variables["v10"][:]      
+Dat_ru=Dat_ru.values.astype("float32")  
+Dat_rv=Dat_rv.values.astype("float32")
+Dat_ru=Dat_ru.reshape(-1,37,55)   
+Dat_rv=Dat_rv.reshape(-1,37,55)
+Dat_ru=np.expand_dims(Dat_ru,axis=-1)
+Dat_rv=np.expand_dims(Dat_rv,axis=-1)
+Dat_ru=np.concatenate([Dat_ru,Dat_rv],axis=3)
+Wind_ARP=np.concatenate([Wind_ARP,Dat_ru],axis=0) 
+
+# Read data from 2018 into Wind_ARP
+daT=xr.open_dataset("Data/WindSpeeduv_ARP_2018.nc")
+Dat_ru=daT.variables["u10"][:]      
+Dat_rv=daT.variables["v10"][:]      
+Dat_ru=Dat_ru.values.astype("float32")  
+Dat_rv=Dat_rv.values.astype("float32")
+Dat_ru=Dat_ru.reshape(-1,37,55)   
+Dat_rv=Dat_rv.reshape(-1,37,55)
+Dat_ru=np.expand_dims(Dat_ru,axis=-1)
+Dat_rv=np.expand_dims(Dat_rv,axis=-1)
+Dat_ru=np.concatenate([Dat_ru,Dat_rv],axis=3)
+Wind_ARP=np.concatenate([Wind_ARP,Dat_ru],axis=0) 
+
+# Read data from 2019 into Wind_ARP
+daT=xr.open_dataset("Data/WindSpeeduv_ARP_2019.nc")
+Dat_ru=daT.variables["u10"][:]      
+Dat_rv=daT.variables["v10"][:]      
+Dat_ru=Dat_ru.values.astype("float32")  
+Dat_rv=Dat_rv.values.astype("float32")
+Dat_ru=Dat_ru.reshape(-1,37,55)   
+Dat_rv=Dat_rv.reshape(-1,37,55)
+Dat_ru=np.expand_dims(Dat_ru,axis=-1)
+Dat_rv=np.expand_dims(Dat_rv,axis=-1)
+Dat_ru=np.concatenate([Dat_ru,Dat_rv],axis=3)
+Wind_ARP=np.concatenate([Wind_ARP,Dat_ru],axis=0) 
+
+# Read data from 2020 into Wind_ARP
+daT=xr.open_dataset("Data/WindSpeeduv_ARP_2020.nc")
+Dat_ru=daT.variables["u10"][:]      
+Dat_rv=daT.variables["v10"][:]      
+Dat_ru=Dat_ru.values.astype("float32")  
+Dat_rv=Dat_rv.values.astype("float32")
+Dat_ru=np.delete(Dat_ru,np.s_[472:480],axis=0)      # (31+28)*8+1=473
+Dat_rv=np.delete(Dat_rv,np.s_[472:480],axis=0)      # #(31+29)*8=480
+Dat_ru=Dat_ru.reshape(-1,37,55)   
+Dat_rv=Dat_rv.reshape(-1,37,55)
+Dat_ru=np.expand_dims(Dat_ru,axis=-1)
+Dat_rv=np.expand_dims(Dat_rv,axis=-1)
+Dat_ru=np.concatenate([Dat_ru,Dat_rv],axis=3)
+Wind_ARP=np.concatenate([Wind_ARP,Dat_ru],axis=0) 
+
+# Read data from 2021 into Wind_ARP
+daT=xr.open_dataset("Data/WindSpeeduv_ARP_2021.nc")
+Dat_ru=daT.variables["u10"][:]      
+Dat_rv=daT.variables["v10"][:]      
+Dat_ru=Dat_ru.values.astype("float32")  
+Dat_rv=Dat_rv.values.astype("float32")
+Dat_ru=Dat_ru.reshape(-1,37,55)   
+Dat_rv=Dat_rv.reshape(-1,37,55)
+Dat_ru=np.expand_dims(Dat_ru,axis=-1)
+Dat_rv=np.expand_dims(Dat_rv,axis=-1)
+Dat_ru=np.concatenate([Dat_ru,Dat_rv],axis=3)
+Wind_ARP=np.concatenate([Wind_ARP,Dat_ru],axis=0) 
+
+# Read data from 2022 into Wind_ARP
+daT=xr.open_dataset("Data/WindSpeeduv_ARP_2022.nc")
+Dat_ru=daT.variables["u10"][:]      
+Dat_rv=daT.variables["v10"][:]      
+Dat_ru=Dat_ru.values.astype("float32")  
+Dat_rv=Dat_rv.values.astype("float32")
+Dat_ru=Dat_ru.reshape(-1,37,55)   
+Dat_rv=Dat_rv.reshape(-1,37,55)
+Dat_ru=np.expand_dims(Dat_ru,axis=-1)
+Dat_rv=np.expand_dims(Dat_rv,axis=-1)
+Dat_ru=np.concatenate([Dat_ru,Dat_rv],axis=3)
+Wind_ARP=np.concatenate([Wind_ARP,Dat_ru],axis=0) 
+
+# Read data from 2023 into Wind_ARP
+daT=xr.open_dataset("Data/WindSpeeduv_ARP_2023.nc")
+Dat_ru=daT.variables["u10"][:]      
+Dat_rv=daT.variables["v10"][:]      
+Dat_ru=Dat_ru.values.astype("float32")  
+Dat_rv=Dat_rv.values.astype("float32")
+Dat_ru=Dat_ru.reshape(-1,37,55)   
+Dat_rv=Dat_rv.reshape(-1,37,55)
+Dat_ru=np.expand_dims(Dat_ru,axis=-1)
+Dat_rv=np.expand_dims(Dat_rv,axis=-1)
+Dat_ru=np.concatenate([Dat_ru,Dat_rv],axis=3)
+Wind_ARP=np.concatenate([Wind_ARP,Dat_ru],axis=0) 
+
+# Mask the datasets
+ARP_bd=pd.read_csv("Data/ARP_Boundary.csv")                       # Import ARP polygon boundary
+ARP_polygon=list(zip(ARP_bd["x_dense"],ARP_bd["y_dense"]))        
+poly_path=Path(ARP_polygon)                                       # Connect boundary points in order and form a polygon 
+inside_flat=poly_path.contains_points(Dat_loc,radius=-1e-9)       # Test if points in Dat_loc are inside the path, radius=-1e-9 means we include the points on path
+inside_mask_2d=inside_flat.reshape(37, 55)                        # Reshape the mask 
+ARP_mask=inside_mask_2d.astype(Wind_ARP.dtype)  # Convert (TRUE,FALSE) to (1.0,0.0) 
+ARP_mask=ARP_mask[np.newaxis,:,:,np.newaxis]    # Add dimensions to ARP_mask so that it can match the data tensor 
+Wind_ARP=Wind_ARP*ARP_mask
+
+# Standardize the datasets
+Wind_mean=Wind_ARP.mean(axis=(0,1,2),keepdims=True)    # Compute mean over samples and space, per channel
+Wind_std=Wind_ARP.std(axis=(0,1,2),keepdims=True)      # Compute  over samples and space, per channel
+Wind_ARP=(Wind_ARP-Wind_mean)/Wind_std
+
+N=Wind_ARP.shape[0]
+np.random.seed(123)
+perm=np.random.permutation(N)                   # shuffle the dataset
+x_train=Wind_ARP[perm[:int(0.5*N)]]             # split the data 
+x_val=Wind_ARP[perm[int(0.5*N):]]
+
+
+#%% Construct VAE and optimizer
+# Creating a sampling layer 
+class Sampling(layers.Layer):           # define a custom keras layer for sampling
+    """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
+
+    def __init__(self, **kwargs):       # _init_: initialize the layer; **kwargs: pass any keyword argument to this layer
+        super().__init__(**kwargs)      # super()._init_: set up keras layer internals
+        self.seed_generator=keras.random.SeedGenerator(1337)   # fix seed for reproducibility
+
+    def call(self, inputs):
+        z_mean, z_log_var=inputs      # outputs of encoder, the \mu(x) and \log(\sigma(x)^2)
+        batch=ops.shape(z_mean)[0]    # number of samples in mini-batch
+        dim=ops.shape(z_mean)[1]      # dimension of latent vactor
+        epsilon=keras.random.normal(shape=(batch,dim),seed=self.seed_generator) # generate epsilon
+        return z_mean+ops.exp(0.5*z_log_var)*epsilon      # transfer to sampled latent vector
+    
+
+# Build the encoder
+latent_dim=64                                 # dimension of latent vector: 2A
+encoder_inputs=keras.Input(shape=(37,55,2))   # each input is an image of size 37 * 55 * 2 
+x=layers.Conv2D(32,3,activation="relu",strides=2,padding="same")(encoder_inputs)
+x=layers.Conv2D(64,5,activation="relu",strides=2,padding="same")(x)
+x=layers.Flatten()(x)
+x=layers.Dense(1024,activation="relu")(x)
+z_mean=layers.Dense(latent_dim,name="z_mean")(x)
+z_log_var=layers.Dense(latent_dim,name="z_log_var")(x)
+z=Sampling()([z_mean,z_log_var])              # sample latent vector z
+encoder=keras.Model(encoder_inputs,[z_mean,z_log_var,z],name="encoder")  # build the encoder model
+encoder.summary()
+
+
+# Build the decoder
+latent_inputs=keras.Input(shape=(latent_dim,))
+x=layers.Dense(1024,activation="relu")(latent_inputs)
+x=layers.Dense(37*55*96,activation="relu")(x)
+x=layers.Reshape((37,55,96))(x)
+x = layers.Conv2D(96,5,activation="relu",padding="same")(x)
+decoder_outputs=layers.Conv2D(2,3,padding="same")(x)
+decoder=keras.Model(latent_inputs, decoder_outputs, name="decoder")     # build the decoder
+decoder.summary()
+
+
+# Define the VAE as a Model with a custom train_step
+class VAE(keras.Model):
+    def __init__(self, encoder, decoder, **kwargs):   # pass in encoder and decoder built before
+        super().__init__(**kwargs)
+        self.encoder=encoder          # internal model in VAE
+        self.decoder=decoder          # internel model in VAE
+        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")    # track total loss
+        self.reconstruction_loss_tracker = keras.metrics.Mean(
+            name="reconstruction_loss"
+        )                                                                  # track recnstruction loss
+        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")          # track KL loss
+
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,              # keras uses this to know which metric to reset each epoch and which metric to log
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker,
+        ]
+
+    def train_step(self, data):                   # define one training update using one mini-batch, where data are of size (batch_size,28,28,1)
+        with tf.GradientTape() as tape:           # record operations in this block for gradient
+            z_mean, z_log_var, z = self.encoder(data)  # encode
+            reconstruction = self.decoder(z)           # decode
+            reconstruction_loss = ops.mean(            # calculate the reconstruction loss using "BCE"
+                ops.sum(
+                    ops.square(data-reconstruction),
+                    axis=(1,2,3),
+                )
+            )
+            kl_loss=-0.5*(1+z_log_var-ops.square(z_mean)-ops.exp(z_log_var)) # calculate the KL divergence
+            kl_loss=ops.mean(ops.sum(kl_loss,axis=1))
+            total_loss = reconstruction_loss +0.7* kl_loss
+        grads=tape.gradient(total_loss,self.trainable_weights)    # calculate gradient
+        self.optimizer.apply_gradients(zip(grads,self.trainable_weights)) # tell the optimizer to update each weight/parameter using its corresponding gradient
+        self.total_loss_tracker.update_state(total_loss)             # keep track of the average loss across all batches so far
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
+    
+
+#%% Train the VAE and test on validation set
+vae=VAE(encoder, decoder)           # wrap the encoder and decoder using the VAE model class defined before
+vae.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001))  # choose the optimizer and compile
+vae.fit(x_train,epochs=30,batch_size=32) # train the vae
+z_mean_val, z_log_var_val, z_val =vae.encoder.predict(x_val, verbose=0)
+x_val_recon=vae.decoder.predict(z_val,verbose=0)
+val_recon_loss=tf.reduce_mean(tf.reduce_sum(tf.square(x_val-x_val_recon),axis=(1,2,3))).numpy()
+val_kl_loss=tf.reduce_mean(tf.reduce_sum(-0.5*(1+z_log_var_val-tf.square(z_mean_val)-tf.exp(z_log_var_val)),axis=1)).numpy()
+val_total_loss=val_recon_loss+0.7*val_kl_loss
+print(val_recon_loss,val_kl_loss,val_total_loss)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
